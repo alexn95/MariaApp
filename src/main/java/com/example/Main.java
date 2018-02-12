daa11099c935b18d4f4aa9dd3574f214d534a27e
alexey <mr.alexn95@MAIL.RU>
2018-02-12 18:55:40 +0300

    mess

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -90,7 +90,7 @@ public class Main {
             Statement stmt = connection.createStatement();
 //            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
 //            stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-            ResultSet rs = stmt.executeQuery("SELECT tick FROM orders");
+            ResultSet rs = stmt.executeQuery("SELECT * FROM orders");
 
             ArrayList<String> output = new ArrayList<String>();
             while (rs.next()) {
