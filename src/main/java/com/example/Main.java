7b4c2ef5167d3fd84757e0b3c287a8e376651ca8
alexey <mr.alexn95@MAIL.RU>
2018-02-13 18:53:20 +0300

    refactoring

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,114 +1,100 @@
-///*
-// * Copyright 2002-2014 the original author or authors.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *      http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
-//package com.example;
-//
-//import com.zaxxer.hikari.HikariConfig;
-//import com.zaxxer.hikari.HikariDataSource;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-//import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoDefaultConfiguration;
-//import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoProperties;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.sql.DataSource;
-//import java.sql.Connection;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.ArrayList;
-//import java.util.Map;
-//
-//import static javax.measure.unit.SI.KILOGRAM;
-//import javax.measure.quantity.Mass;
-//import javax.xml.ws.Response;
-//
-//import org.jscience.physics.model.RelativisticModel;
-//import org.jscience.physics.amount.Amount;
-//import org.springframework.web.servlet.ModelAndView;
-//import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-//
-//
-//@ComponentScan
-//@SpringBootApplication
-//public class Main {
-//
-//    public static void main(String[] args) throws Exception {
-//        SpringApplication.run(Main.class, args);
-//    }
-//
-//    @Value("${spring.datasource.url}")
-//    private String dbUrl;
-//
-//    @Bean
-//    public DataSource dataSource() throws SQLException {
-//        if (dbUrl == null || dbUrl.isEmpty()) {
-//            return new HikariDataSource();
-//        } else {
-//            HikariConfig config = new HikariConfig();
-//            config.setJdbcUrl(dbUrl);
-//            return new HikariDataSource(config);
-//        }
-//    }
-//
-//    @EnableGlobalMethodSecurity(prePostEnabled = true)
-//    protected static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
-//        @Override
-//        protected MethodSecurityExpressionHandler createExpressionHandler() {
-//            return new OAuth2MethodSecurityExpressionHandler();
-//        }
-//    }
-//
-//
-//    @Configuration
-//    @EnableOAuth2Sso
-//    static class ExampleSecurityConfigurerAdapter extends OAuth2SsoDefaultConfiguration {
-//
-//        public ExampleSecurityConfigurerAdapter(ApplicationContext applicationContext, OAuth2SsoProperties sso) {
-//            super(applicationContext, sso);
-//        }
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//
-//            // In this example we allow anonymous access to the root index page
-//            // this MUST be configured before calling super.configure
-//            http.authorizeRequests()
-//                    .antMatchers("/").permitAll()
-//                    .and().formLogin().loginPage("/login").permitAll();
-//
-//            // calling super.configure locks everything else down
-//            super.configure(http);
-//            // after calling super, you can change the logout success url
-//            http.logout().logoutSuccessUrl("/");
-//        }
-//    }
-//
-//}
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoDefaultConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+
+import javax.sql.DataSource;
+
+import java.sql.SQLException;
+
+
+
+@ComponentScan
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
+
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    protected static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+        @Override
+        protected MethodSecurityExpressionHandler createExpressionHandler() {
+            return new OAuth2MethodSecurityExpressionHandler();
+        }
+    }
+
+
+    @Configuration
+    @EnableOAuth2Sso
+    static class ExampleSecurityConfigurerAdapter extends OAuth2SsoDefaultConfiguration {
+
+        public ExampleSecurityConfigurerAdapter(ApplicationContext applicationContext, OAuth2SsoProperties sso) {
+            super(applicationContext, sso);
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            // In this example we allow anonymous access to the root index page
+            // this MUST be configured before calling super.configure
+            http.authorizeRequests()
+                    .antMatchers("/").permitAll()
+                    .and().formLogin().loginPage("/login").permitAll();
+
+            // calling super.configure locks everything else down
+            super.configure(http);
+            // after calling super, you can change the logout success url
+            http.logout().logoutSuccessUrl("/");
+        }
+    }
+
+}
