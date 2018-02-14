a92757595f8a7f1cfe92a369c887fee0487bdc91
alexey <mr.alexn95@MAIL.RU>
2018-02-14 16:42:37 +0300

    orders list

--- src/main/resources/public/js/custom/order_complete_change.js
+++ src/main/resources/public/js/custom/order_complete_change.js
@@ -1,15 +1,6 @@
     $(document).ready(function () {
            $('.isComplete:checkbox').each(function () {
                $(this).change(function(info){
-                   var csrfParameter = $("meta[name='_csrf_parameter']").attr("content");
-                   var csrfToken = $("meta[name='_csrf']").attr("content");
-                   var csrfHeader = $("meta[name='_csrf_header']").attr("content");
-                   var data = {}
-                   var headers = {}
-                   data[csrfParameter] = csrfToken;
-                   data[id] = id;
-                   data[isComplete] = isComplete;
-                   headers[csrfHeader] = csrfToken;
                     var id = $(this).attr('id');
                     var isComplete = false;
                     if(this.checked) {
@@ -17,9 +8,8 @@
                     }
                     $.ajax({
                         type:"POST",
-                        data: data,
+                        data:{id, isComplete},
                         url:"orders",
-                        headers: headers,
                         success: function(data){
                              console.log(data);
                         }
