ac39c7fa8dd44ef3f442265fc5af0d060edd023a
alexey <mr.alexn95@MAIL.RU>
2018-02-14 17:46:28 +0300

    orders list

--- src/main/resources/public/js/custom/ajax_order.js
+++ src/main/resources/public/js/custom/ajax_order.js
@@ -4,9 +4,18 @@ $(document).ready(function () {
                var name = $('#recipient_name').val();
                var phone = $('#recipient_phone').val();
                var note = $('#recipient_note').val();
+
+               var csrfParameter = $("meta[name='_csrf_parameter']").attr("content");
+               var csrfToken = $("meta[name='_csrf']").attr("content");
+               var csrfHeader = $("meta[name='_csrf_header']").attr("content");  // THIS WAS ADDED
+               var data = {surname, name, phone, note};
+               var headers = {};
+               data[csrfParameter] = csrfToken;
+               headers[csrfHeader] = csrfToken;
+
                $.ajax({
                    type:"POST",
-                   data:{surname, name, phone, note},
+                   data: data,
                    url:"",
                    success: function(data){
                        console.log(data);
