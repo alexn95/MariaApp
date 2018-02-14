376ebac207b6c7ad2f4ad66e81d3d4654be44148
alexey <mr.alexn95@MAIL.RU>
2018-02-14 20:41:45 +0300

    orders list

--- src/main/resources/public/js/custom/orders.js
+++ src/main/resources/public/js/custom/orders.js
@@ -1,6 +1,8 @@
     $(document).ready(function () {
            $('.isComplete:checkbox').each(function () {
                $(this).change(function(info){
+
+
                     var id = $(this).attr('id');
                     var isComplete = false;
                     if(this.checked) {
